dd8b7e46417a3c11a946d8fc24e53c51bcfabb57
Jackson Ruckar <workingasintendead@gmail.com>
2025-05-03 14:18:26 -0700

    tweaks...

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,16 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
 
 export const config = {
   matcher: ['/(.*)'],
 };
 
-export async function middleware(request: NextRequest) {
-  const maintenance = await get<{ enabled: boolean }>('maintenance');
-  const isMaintenancePath = request.nextUrl.pathname.startsWith('/maintenance');
+export async function middleware(req: NextRequest) {
+  const maintenance = await get('maintenance');
 
-  if (
-    maintenance?.enabled &&
-    !isMaintenancePath &&
-    !request.nextUrl.pathname.startsWith('/_next')
-  ) {
-    return NextResponse.redirect(new URL('/maintenance', request.url));
+  if (maintenance) {
+    req.nextUrl.pathname = `/maintenance`;
+
+    return NextResponse.rewrite(req.nextUrl);
   }
-
-  return NextResponse.next();
 }
