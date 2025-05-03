a0f73a57d922f7f709e72ce856615ad30fc90983
Jackson Ruckar <workingasintendead@gmail.com>
2025-05-03 13:30:33 -0700

    added vercel.json as needed

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,8 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const maintenanceMode = process.env.MAINTENANCE_MODE === 'true';
   const isMaintenancePath = request.nextUrl.pathname.startsWith('/maintenance');
-  const isProduction = process.env.NODE_ENV === 'production';
 
   if (
-    isProduction &&
     maintenanceMode &&
     !isMaintenancePath &&
     !request.nextUrl.pathname.startsWith('/_next')
