220bcb61c776c0c6a06b1288100f5c0167468fd9
Jackson Ruckar <workingasintendead@gmail.com>
2025-05-03 14:10:13 -0700

    using edge instead or json

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { get } from '@vercel/edge-config';
 
-export function middleware(request: NextRequest) {
-  const maintenanceMode = process.env.MAINTENANCE_MODE === 'true';
+export const config = {
+  matcher: ['/(.*)'],
+};
+
+export async function middleware(request: NextRequest) {
+  const maintenance = await get<{ enabled: boolean }>('maintenance');
   const isMaintenancePath = request.nextUrl.pathname.startsWith('/maintenance');
 
   if (
-    maintenanceMode &&
+    maintenance?.enabled &&
     !isMaintenancePath &&
     !request.nextUrl.pathname.startsWith('/_next')
   ) {
@@ -15,7 +20,3 @@ export function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-};
