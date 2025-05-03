4746393aa0b71f2a6c5d104ea73e088bb796eee1
Jackson Ruckar <workingasintendead@gmail.com>
2025-05-03 12:46:16 -0700

    about page started with a clientlist component and data to pull from. updated background to always be black

---
+++ src/components/ClientList.tsx
@@ -0,0 +1,26 @@
+type ClientListProps = {
+  clients: string[];
+};
+
+function ClientList({ clients }: ClientListProps) {
+  const half = Math.ceil(clients.length / 2);
+  const column1 = clients.slice(0, half);
+  const column2 = clients.slice(half);
+
+  return (
+    <div className="flex justify-center gap-4 text-lg">
+      <div className="flex flex-col gap-1 w-32 text-right">
+        {column1.map((client) => (
+          <div key={client}>{client}</div>
+        ))}
+      </div>
+      <div className="flex flex-col gap-1 w-32 text-left">
+        {column2.map((client) => (
+          <div key={client}>{client}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ClientList;
---
+++ src/data/clients.ts
@@ -0,0 +1,24 @@
+const clients = [
+  'Allbirds',
+  'AP Magazine',
+  'Apple',
+  'Asus',
+  'Canon',
+  'CrossFit',
+  'Dell',
+  'DJI',
+  'LG',
+  'MSI',
+  'Nike',
+  'Nikon',
+  'Olympus',
+  'Panasonic',
+  'Peloton',
+  'Razer',
+  'Sony',
+  'Samsung',
+  'USA Today',
+  'Whoop',
+];
+
+export default clients;
