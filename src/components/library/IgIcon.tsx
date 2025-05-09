e30ad389d0ab50c090c655cd46924f7f83ffdbab
Jackson Ruckar <workingasintendead@gmail.com>
2025-05-09 16:33:28 -0700

    just a few tailwind fixes on navbar

--- src/components/library/IgIcon.tsx
+++ src/components/library/IgIcon.tsx
@@ -10,7 +10,7 @@ const IgIcon: React.FC<IgIconProps> = ({ size = 24 }) => (
     href="https://www.instagram.com/jacksonruckar"
     target="_blank"
     rel="noopener noreferrer"
-    className="text-white hover:text-pink-500 transition-colors duration-500"
+    className="hover:text-pink-500 transition-colors duration-500"
     aria-label="Jackson Ruckar's Instagram"
   >
     <FaInstagram size={size} />
